f35f8f0ade4344134dfd77daca7124ffadf58828
KNXKO <lukaslechovic@gmail.com>
2024-02-16 23:15:36 +0100

    Added code comments

--- CleanDisk.py
+++ CleanDisk.py
@@ -1,33 +1,41 @@
+# Library
 import threading
-import pygetwindow as gw
-import ctypes
+import threading
 import tkinter as tk
-from tkinter import messagebox
 from functools import partial
+# Import functions
 from functions import disk_cleanup, prefetch, win_update, ms_store_update, \
     defrag, temp, learix_fps, adobe, word, ticktick, signalrgb, ccleaner, close_apps, \
     nvidia, drivers_link
 
+# Define stop_event as a global variable
+stop_event = threading.Event()
+
+# Stop all functions
 def stop_all_functions():
     stop_event.set()
     print("All functions execution stopped.")
 
+# Start all functions
 def run_selected_functions(functions_to_run):
     for func_name in functions_to_run:
         func = functions_mapping.get(func_name)
         if func:
             func()
 
+# Stop all functions when pressing "q"
 def on_key(event):
     if event.char.lower() == "q":
         stop_all_functions()
 
+# Run spec function
 def run_function(func):
     try:
         func()
     except Exception as e:
         print(f"Error running function: {e}")
 
+# Styles
 def update_colors():
     root.config(bg="#292929")
     label.config(bg=bg_color, fg=fg_color)
@@ -44,14 +52,16 @@ def update_colors():
 root = tk.Tk()
 root.title("KNX")
 root.resizable(False, False)
-root.dark_mode = True
 root.option_add("*Font", "Calibri")
+# TOP Text
 label = tk.Label(root, text="KNX Cleaner & Updater", font=("Calibri", 13))
 label.pack()
 
+# Style: Create function buttons frame
 function_buttons_frame = tk.Frame(root)
 function_buttons_frame.pack(expand=False, pady=10)
 
+# Function buttons
 functions_mapping = {
     "Run Learix FPS Script": learix_fps.learix_fps,
     "Run Disk Cleanup": disk_cleanup.run_disk_cleanup,
@@ -70,6 +80,7 @@ functions_mapping = {
     "Open CCleaner": ccleaner.open_ccleaner,
 }
 
+# Style:
 for function_name, func in functions_mapping.items():
     button_frame = tk.Frame(function_buttons_frame)
     button_frame.pack(side=tk.TOP, fill=tk.X, padx=5, pady=2)
@@ -81,6 +92,7 @@ for function_name, func in functions_mapping.items():
     fg_color = "#A9A9A9"
     button.configure(bg=bg_color, fg=fg_color)
 
+# Bottom buttons
 all_functions_button = tk.Button(root, text="Run all", command=partial(run_selected_functions, functions_mapping.keys()))
 all_functions_button.pack(side=tk.TOP, fill=tk.X, padx=5, pady=2)
 
