edcf1369ebb2bae7ad8c9c0bac38314c2f92e55e
KNXKO <lukaslechovic@gmail.com>
2024-03-05 18:56:59 +0100

    Update sfc scannow script

--- functions/sfc_scannow.py
+++ functions/sfc_scannow.py
@@ -1,9 +1,26 @@
-import subprocess
+import os
+import pyautogui
+import time
+import pygetwindow as gw
+
+cmd_path = r'AssetsScripts\cmd.lnk'
 
 def run_sfc_scan():
     try:
-        # Spustenie externeho cmd okna a vykonanie prikazu sfc /scannow
-        subprocess.run("cmd /c sfc /scannow", shell=True)
+        os.startfile(cmd_path)
+        print("Otvorený CMD")
+        time.sleep(2)
+        while True:
+            active_window = gw.getWindowsWithTitle("Administrator: cmd")
+            if active_window:  # Ak sme našli okno
+                active_window = active_window[0]
+                break
+            if not active_window:
+                print("Okno príkazového riadku nebolo nájdené.")
+                return
+        active_window.activate()
+        pyautogui.write('sfc /scannow')
+        pyautogui.press('enter')
         print("SFC skenovanie bolo úspešne spustené.")
     except Exception as e:
-        print(f"Chyba pri vykonávaní príkazu sfc /scannow: {e}")
+        print("Chyba pri otváraní CMD alebo spúšťaní sfc /scannow:", str(e))
